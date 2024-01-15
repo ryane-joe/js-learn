5a0cb24b53e4c587b95a19fa33c90afe70ff758f
ryane joe <125251551+ryane-joe@users.noreply.github.com>
2024-01-15 07:47:28 +0000

    switching to vsc desktop

---
+++ basics/datatypes.js
@@ -0,0 +1,4 @@
+"use strict" ;//only use new js
+
+//alert("hello") doesnt work with node js/ non browser 
+
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const account_ID = 6969; // cant change value once set
+let account_Email = "mommy@69.feet" // can change value once set
+let area = "epstines island"
+
+//account_ID = 420 [cant because of constant variable]
+//console.log(account_ID)
+//preffer not to use var
+console.table([account_ID,account_Email,area]) // prints out in table format
