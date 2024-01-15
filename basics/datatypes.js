81ba9f0630d4b08efcbf73bb500cdc8debcfcd84
ryane joe <ryanejoe@proton.me>
2024-01-15 13:34:11 +0530

    datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,4 +1,18 @@
 "use strict" ;//only use new js
 
 //alert("hello") doesnt work with node js/ non browser 
+let name = "Jhonny ykwim" //strings
+let age = 420 //integers
+//max length of number 2 ** 52
+let sex = true //true / false values (Boolean)
+//null =>stand alone value
+let alive_or_not; //undefined
+let soul = null // empty value(me inside)
+ 
+console.log(typeof age) //prints the datatype
+console.log(typeof null) //prints datatype "object"
 
+/*
+used for documentation of datatypes and values standards:
+https://262.ecma-international.org/14.0/#sec-ecmascript-data-types-and-values
+*/
